7a46b2a39bfd12d3575361ea62b96b72016a3399
Pietro Ruggia <pietroruggia02@gmail.com>
2024-05-16 16:36:20 +0200

    login e register completati

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,27 @@
+<x-layout>
+    <div class="container-fluid text-primary">
+        <div class="row justify-content-center">
+            <div class="col-12 text-center">
+                <h1 class="display-1 mb-5 mt-5">Accedi</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-8 border p-5 shadow rounde">
+                <form method="POST" action="{{route('login')}}">
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label">Email</label>
+                        <input type="email" class="form-control" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-layout>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,35 @@
+<x-layout>
+    <div class="container-fluid text-primary">
+        <div class="row justify-content-center">
+            <div class="col-12 text-center">
+                <h1 class="display-1 mb-5 mt-5">Registrati</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-8 border p-5 shadow rounde">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label">Username</label>
+                        <input type="text" class="form-control" name="name">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Email</label>
+                        <input type="email" class="form-control" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Conferma Password</label>
+                        <input type="password" class="form-control" name="password_confirmation">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-layout>
