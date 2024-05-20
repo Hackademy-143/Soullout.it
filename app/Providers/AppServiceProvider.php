897628cf88d8df012770865d5461d9fd05b57e02
Alexandra Busuioc <alexandrabusuioc27@yahoo.com>
2024-05-20 10:14:47 +0200

    provider modificato

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Category;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $categories = Category::all();
-        View::share('categories', $categories);
-
+        if ( Schema::hasTable('categories') ){
+        View::share('categories', Category::all());
+        }
     }
 }
