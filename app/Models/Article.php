10be8d069a2823bee9eac449045235bb12e7630e
CortanaEly <117460669+CortanaEly@users.noreply.github.com>
2024-05-22 12:57:31 +0200

    inizio serch

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\Category;
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Article extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $fillable =[
 
@@ -30,5 +33,17 @@ class Article extends Model
     }
     public static function toBeRevisedCount(){
     return Article::where('is_accepted', null)->count();
+    }
+
+    public function toSearchableArray()
+    {
+        return[
+            'id'=>$this->id,
+            'nome'=>$this->nome,
+            'provenienza'=>$this->provenienza,
+            'descrizione'=>$this->descrizione,
+            'categoria'=>$this->category,
+            'prezzo'=>$this->prezzo
+        ];
     }
 }
